b38d505043e7016b92b9e0e520e6a84c3c308e8b
tdmahaff <151571455+tdmahaff@users.noreply.github.com>
2026-09-11 18:40:34 -0600

    Add files via upload

---
+++ one_1.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.liang_1;
+
+/**
+ *
+ * @author Owner
+ */
+public class one_1 {
+    
+    public static void main(String[] args){
+        
+        System.out.println("Welcome to Java");
+        System.out.println("Welcome to Computer Science");
+        System.out.println("Programming is fun");
+        
+    }
+    
+}
