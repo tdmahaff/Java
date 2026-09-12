27e24ccd83c27ba2521679ba353f918da58fd84d
tdmahaff <151571455+tdmahaff@users.noreply.github.com>
2026-09-11 20:22:39 -0600

    Add files via upload

---
+++ one_5.java
@@ -0,0 +1,12 @@
+//(Compute expressions) Write a program that displays the result of
+
+public class one_5 {
+
+    public static void main(String[] args){
+
+        System.out.println(((9.5*4.5) - (2.5*3)) / (45.5-3.5));
+
+    }
+
+
+}
