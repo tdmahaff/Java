f45ab3358fad1de0b3d70f9ee410ea84a2d645f5
tdmahaff <151571455+tdmahaff@users.noreply.github.com>
2026-09-11 20:26:02 -0600

    Add files via upload

---
+++ one_6.java
@@ -0,0 +1,22 @@
+// (Summation of a series) Write a program that displays the result of
+//1 + 2 + 3 + 4 + 5 + 6 + 7 + 8 + 9.
+
+public class one_6 {
+
+    public static void main(String[] args){
+
+        int sum = 0;
+
+        for(int i = 0; i < 10; i++){
+
+            sum += i;
+
+        }
+
+        System.out.printf("Sum is %d", sum);
+
+
+    }
+
+
+}
