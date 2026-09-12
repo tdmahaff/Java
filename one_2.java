91c50a42bf28b7fcde5c32869a4576c3c0cc56b8
tdmahaff <151571455+tdmahaff@users.noreply.github.com>
2026-09-11 18:45:17 -0600

    Add files via upload

---
+++ one_2.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.liang_1;
+
+/**
+ *
+ * (Display five messages) Write a program that displays Welcome to Java five
+times
+ * @author Owner
+ */
+public class one_2 {
+    
+    public static void main(String[] args){
+        
+        int i = 0;
+        while(i < 5){
+            
+            System.out.println("Welcome to Java");
+            i++;
+            
+        }
+        
+        
+    }
+    
+}
