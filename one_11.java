81da3dc1b707567d799cc0d640b893e87b057c61
tdmahaff <151571455+tdmahaff@users.noreply.github.com>
2026-09-11 20:58:01 -0600

    Add files via upload

---
+++ one_11.java
@@ -0,0 +1,33 @@
+/*
+(Population projection) The U.S. Census Bureau projects population based on
+the following assumptions:
+■ ■ One birth every 7 seconds
+■ ■ One death every 13 seconds
+■ ■ One new immigrant every 45 seconds
+Write a program to display the population for each of the next five years. Assume
+that the current population is 312,032,486, and one year has 365 days. Hint: In Java,
+if two integers perform division, the result is an integer. The fractional part is trun-
+cated. For example, 5 / 4 is 1 (not 1.25) and 10 / 4 is 2 (not 2.5). To get an accu-
+rate result with the fractional part, one of the values involved in the division must be
+a number with a decimal point. For example, 5.0 / 4 is 1.25 and 10 / 4.0 is 2.5.
+ */
+
+public class one_11 {
+
+    public static void main(String[] args){
+
+        double seconds_per_year = 365 * 24 * 3600;
+        double births = seconds_per_year / 7.0;
+        double deaths = seconds_per_year / 13.0;
+        double immigrants = seconds_per_year / 45.0;
+        double current_population = 312032486;
+
+        for(int i = 0; i < 5; i++){
+
+            current_population += births - deaths + immigrants;
+            System.out.printf("Population for year %d is %f\n", i + 1, current_population);
+
+        }
+
+    }
+}
